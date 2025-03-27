f880b0cdcf42540435bc869ad0cf60999644ec9c
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-27 14:45:42 +0100

    Initial Commit

---
+++ Facts/FactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactsApp.swift
+//  Facts
+//
+//  Created by Eric on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
