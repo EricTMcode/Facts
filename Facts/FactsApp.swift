368dcda3cd99e7a2d2ceca340c3a8769194d6234
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-27 15:08:32 +0100

    Create EditPersonView

--- Facts/FactsApp.swift
+++ Facts/FactsApp.swift
@@ -6,13 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct FactsApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: Person.self)
         }
-        .modelContainer(for: Person.self)
     }
 }
