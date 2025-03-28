0c11333c2eac2517bed66d3045293290a6362976
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-28 16:39:35 +0100

    Create event swiftData model

---
+++ Facts/Model/Event.swift
@@ -0,0 +1,20 @@
+//
+//  Event.swift
+//  Facts
+//
+//  Created by Eric on 28/03/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Event {
+    var name: String
+    var location: String
+
+    init(name: String, location: String) {
+        self.name = name
+        self.location = location
+    }
+}
